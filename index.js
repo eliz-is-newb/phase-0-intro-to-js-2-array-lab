315ead1c1173e3da6ba3ab8d70e228f64f495381
elizabeth sky degrate <skydegrate@gmail.com>
2022-06-30 06:45:22 -0400

    commit tree 3ee2a7a8c209a0358df1778bdb225d821caf38ff

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catName) {
+    cats.push(catName);
+}
+
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat(catName) {
+    cats.pop(catName);
+}
+
+function destructivelyRemoveFirstCat(catName) {
+    cats.shift(catName);
+}
+
+function appendCat(catName) {
+    return [...cats, catName];
+}
+
+function prependCat(catName) {
+    return [catName, ...cats];
+}
+
+function removeFirstCat(catName) {
+    return cats.slice(1);
+}
+
+function removeLastCat(catName) {
+    return cats.slice(0, cats.length - 1);
+}
+
+
